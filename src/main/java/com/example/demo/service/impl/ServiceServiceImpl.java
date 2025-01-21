598e83a590be6c21209a5625a30f16f3c2382369
Mauro-Entenza <149701802+Mauro-Entenza@users.noreply.github.com>
2025-01-21 18:33:26 +0100

    Acabo con las CRUD principales.

--- src/main/java/com/example/demo/service/impl/ServiceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ServiceServiceImpl.java
@@ -5,10 +5,12 @@ import com.example.demo.domain.entity.Service;
 import com.example.demo.exception.ServiceNotFoundException;
 import com.example.demo.repository.ServiceRepository;
 import com.example.demo.service.ServiceService;
+import jakarta.persistence.criteria.Predicate;
 import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 
 @org.springframework.stereotype.Service
 public class ServiceServiceImpl implements ServiceService {
@@ -40,10 +42,36 @@ public class ServiceServiceImpl implements ServiceService {
   }
 
   @Override
-  public List<ServiceDto> searchServices(String type, Float minPrice, Float maxPrice,
+  public List<ServiceDto> searchServices(String name, Float minPrice, Float maxPrice,
       Boolean isActive) {
-    return null;
+    Specification<Service> specification = (root, query, criteriaBuilder) -> {
+      Predicate predicate = criteriaBuilder.conjunction();
+      if (name != null && !name.isEmpty()) {
+        predicate = criteriaBuilder.and(predicate,
+            criteriaBuilder.like(criteriaBuilder.lower(root.get("name")),
+                "%" + name.toLowerCase() + "%"));
+      }
+      if (minPrice != null) {
+        predicate = criteriaBuilder.and(predicate,
+            criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice));
+      }
+      if (maxPrice != null) {
+        predicate = criteriaBuilder.and(predicate,
+            criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice));
+      }
+      if (isActive != null) {
+        predicate = criteriaBuilder.and(predicate,
+            criteriaBuilder.equal(root.get("isActive"), isActive));
+      }
+
+      return predicate;
+    };
+
+    return serviceRepository.findAll(specification).stream()
+        .map(service -> modelMapper.map(service, ServiceDto.class))
+        .toList();
   }
+
 
   @Override
   public void deleteService(long serviceId) throws ServiceNotFoundException {
